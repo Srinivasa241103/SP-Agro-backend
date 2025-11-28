83cfd6544b4f9f38206a646bb3c3d8e4eea765ce
M.Srinivsas shankar <cherry@MSrinivsass-MacBook-Air.local>
2025-11-29 04:36:49 +0530

    changed index file structure

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import dotenv from 'dotenv';
 import app from './src/app.js';
 
-dotenv.config({
-    path: "./.env",
-});
+// Only load .env if it exists locally
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+}
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on: http://localhost:${PORT}`);
-})
+    console.log(`Server is running on port ${PORT}`);
+});
